57309c48edcba969ff4c973868b917a0380bf06b
Ezek <ezk2442@gmail.com>
2020-08-02 15:09:06 +0200

    preparado para comenzar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
 import React from 'react';
-import Reloj from './componentes/Reloj';
-import './App.css';
+import styled from '@emotion/styled';
+import imagen from './imagenes.png';
+
+
+const Contenedor= styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  @media (min-width: 992px){
+    display:grid;
+    grid-template-columns: repeat(2,1fr);
+    column-gap: 2rem;
+  }
+`;
+
+const Heading = styled.h1`
+  font-family: 'Bebas Neue', cursive;
+  color: #FFF;
+  text-align: left;
+  font-weight: 700;
+  font-size: 50px;
+  margin-top:80px;
+
+  &::after {
+    content:'';
+    width: 100px;
+    height:6px;
+    background-color: #66A2FE;
+    display:block;
+  }
+
+
+`;
+
+const Imagen = styled.img`
+  max-width:100%;
+  margin-top: 5rem;
+`;
+
 
 function App() {
   return (
-    <div className="App">
-     <Reloj texto="Bienvenidos"/>
-    </div>
+    <Contenedor>
+        <div>
+          <Imagen
+            src={imagen}
+            alt="imagen cripto"
+          />
+        </div>
+        <div>
+          <Heading>COTIZA CRIPTOMONEDAS AL INSTANTE</Heading>
+        </div>  
+    </Contenedor>
   );
 }
 
