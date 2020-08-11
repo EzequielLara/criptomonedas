99fddd4bfbeaae54e3eb5f62a842440d9daca806
Ezek <ezk2442@gmail.com>
2020-08-11 20:35:59 +0200

    Añadiendo Styled Components al custom hook

--- src/hooks/useMoneda.js
+++ src/hooks/useMoneda.js
@@ -1,7 +1,29 @@
 import React, { Fragment, useState } from 'react';
+import styled from '@emotion/styled';
 
+const Label = styled.label `
+    font-family: 'Bebas Neue', cursive;
+    color: #FFF;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 2.4rem;
+    margin-top: 2rem;
+    display: block;
+`;
 
-const useMoneda = (label, stateInicial, opciones)=>{
+const Select = styled.select`
+    width: 100%;
+    display: block;
+    padding: 1rem;
+    -webkit-appearance: none;
+    border-radius: 10px;
+    border: none;
+    font-size: 1.2rem;
+
+`;
+
+ 
+const useMoneda = (label, stateInicial, monedas)=>{
 
     //State del custom hook
     const [state, actualizarState] = useState(stateInicial);
@@ -10,13 +32,16 @@ const useMoneda = (label, stateInicial, opciones)=>{
         return(
 
             <Fragment>
-                <label>{label}</label>
-                <select>
-                    <option>Seleccione</option>
-                    {opciones.map(opcion=>(
+                <Label>{label}</Label>
+                <Select
+                    onChange={e =>actualizarState(e.target.value)}
+                    value={state}
+                >
+                    <option value="">-Seleccione-</option>
+                    {monedas.map(opcion=>(
                         <option key={opcion.codigo} value={opcion.codigo}>{opcion.nombre}</option>
                     ))}
-                </select>
+                </Select>
             </Fragment>
             
         );
